609e41c8fa64a3d31151b4790afb2949014a56ec
Harvey <sylv1612@stud.kea.dk>
2022-07-08 12:00:47 +0200

    started on Introduction - no anim

---
+++ components/Introduction.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1 class="main-title"> Welcome, Input ! </h1>
+        <p class="text-align-center"> How can I help you today? </p>
+        <div class="nav-menu">
+            <span class="title"> Career </span>
+            <span class="title"> CV </span>
+            <span class="title"> Contact </span>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.main-title {
+    font-size: 5rem;
+    text-align: center;
+    line-height: 1em;
+}
+
+.text-align-center {
+    text-align: center;
+}
+
+.nav-menu {
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+
+    @media (max-width: 768px)  {
+        flex-flow: column;
+        justify-content: space-around;
+    }
+}
+
+span {
+    list-style-type: none;
+    margin-left: 20%;
+    font-size: 2.5rem;
+    &:hover {
+        text-decoration: underline;
+        cursor: pointer;
+    }
+}
+
+</style>
